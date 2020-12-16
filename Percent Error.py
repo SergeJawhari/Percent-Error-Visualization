b1745e3195ebc634e07e2bfcdeb7e1bcbd53d4f9
SergeJawhari <60945353+SergeJawhari@users.noreply.github.com>
2020-12-16 03:04:50 -0800

    added percent error visualization

---
+++ Percent Error.py
@@ -0,0 +1,22 @@
+import plotly.graph_objects as go
+import pandas as pd
+import plotly.express as px
+
+def percentage_error(approx, exact):
+    error = (((approx - exact) / exact)*100)
+    return error
+
+data = pd.read_csv("EvT.csv")
+
+fig = px.line(data, x=data['Sprint'], y=percentage_error(data['Approximate'], data['Exact']))
+
+fig.update_layout(
+    title="Percent Errors Visualization",
+    xaxis_title="Sprint",
+    yaxis_title="Percent Error",
+    font=dict(
+        size=18,
+    )
+)
+
+fig.write_html('Percent Error.html', auto_open=True)
